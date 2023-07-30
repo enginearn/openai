c51c038ae99d5f1daa86133293017ac223f0fe42
enginearn <enginyaaaaan@gmail.com>
2023-07-30 15:24:56 +0900

    Modified

--- chat_lang_chain_zenn/src/chat_lang_chain_zenn/chat/main.py
+++ chat_lang_chain_zenn/src/chat_lang_chain_zenn/chat/main.py
@@ -2,39 +2,16 @@
 
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
-
+from chat_utils import init_chat, init_messages, select_model, handle_user_input, display_messages, display_cost
 
 def main() -> None:
-    llm = ChatOpenAI()
-
-    st.set_page_config(
-        page_title="Chat with GPT-3", page_icon="🤖"
-    )
-    st.header("Chat with ChatGPT 😘")
-
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
-        ]
-
-    if user_input := st.chat_input("聞きたいことを入力してね！"):
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("ChatGPT is typing..."):
-            response = llm(st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=response.content))
-
-    messages = st.session_state.get("messages", [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message("assistant"):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message("user"):
-                st.markdown(message.content)
-        else:
-            st.write(f"System message: {message.content}")
-
+    init_chat()
+    init_messages()
+    model, temperature = select_model()
+    llm = ChatOpenAI(model=model, temperature=temperature)
+    handle_user_input(llm)
+    display_messages()
+    display_cost()
 
 if __name__ == "__main__":
     main()
